c75d58cee07eef95aeb185c7550471142ee4d91e
Jhun24 <jjanghuni102@naver.com>
2018-06-18 17:59:16 +0900

    safe neck

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var randomstring = require('ranomstring');
+var randomstring = require('randomstring');
 var mongoose = require('mongoose');
 var session = require('express-session');
 
